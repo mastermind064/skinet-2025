7808ac3c7a3c87f365d5e44b2f7494f503c9ff0e
Master Mind <yoyo.triwahyono@gmail.com>
2025-03-10 13:12:46 +0700

    end of section 2

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+//diatas baris ini disebut service. swagger nda dipake, jadi dihapus aja
+var app = builder.Build();
+// dibawah ini disebut middleware. yang dipake cuman MapControllers dan Run
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
