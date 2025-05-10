03f7e48c24e5ca798d666bd03af67deb77d03727
0601206787 yoyok <yoyok.wahyono@pajak.go.id>
2025-05-10 21:07:24 +0700

    section 9 otw

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,32 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 //pada event oninit, gunakan httpget product
-export class AppComponent implements OnInit{
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    //http balikannya observable object. akan jalan klo di subscribe dulu
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    })
-  }
-  baseUrl = 'https://localhost:5001/api/'
-  //ato bisa juga cara injext httpclient seperti ini
-  private http = inject(HttpClient);
+export class AppComponent {
   title = 'Skinet';
 
 }
---
+++ client/src/app/features/shop/shop.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../core/services/shop.service';
+import { Product } from '../../shared/models/product';
+import { MatCard } from '@angular/material/card';
+import { ProductItemComponent } from "./product-item/product-item.component";
+
+@Component({
+  selector: 'app-shop',
+  imports: [
+    MatCard,
+    ProductItemComponent
+],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss'
+})
+export class ShopComponent implements OnInit{
+  private shopService = inject(ShopService)
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    //http balikannya observable object. akan jalan klo di subscribe dulu
+    this.shopService.getProducts().subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error)
+    })
+  }
+}
