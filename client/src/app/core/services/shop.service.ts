39154fe26e6ed5798229a4237c30e14e4a7c752b
0601206787 yoyok <yoyok.wahyono@pajak.go.id>
2025-05-23 16:52:13 +0700

    End of section 10

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -40,6 +40,10 @@ export class ShopService {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {params})
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id)
+  }
+
   //karena langsung subsrcibe, akan dieksekusi ketika pertama kali app hidup
   //perlu pasang filter biar hanya dieksekusi sekali aja
   getBrands(){
