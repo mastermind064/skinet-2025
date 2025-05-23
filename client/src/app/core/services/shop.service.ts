06cf9e6c9011cd181924fadafd13fc55971f2c0b
0601206787 yoyok <yoyok.wahyono@pajak.go.id>
2025-05-23 14:57:32 +0700

    End of section 9

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Pagination } from '../../shared/models/pagination';
 import { Product } from '../../shared/models/product';
+import { ShopParams } from '../../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,48 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/'
   //ato bisa juga cara injext httpclient seperti ini
   private http = inject(HttpClient);
+  types: string[] = []; //nyimpen data di variabel ini biar ada terus selama app hidup
+  brands: string[] = []; //nyimpen data di variabel ini biar ada terus selama app hidup
 
-  getProducts() {
-    return this.http.get<Pagination<Product>>(this.baseUrl + 'products?pageSize=20')
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams()
+
+    if (shopParams.brands && shopParams.brands.length> 0){
+      params = params.append('brands', shopParams.brands.join(','))
+    }
+
+    if (shopParams.types && shopParams.types.length> 0){
+      params = params.append('types', shopParams.types.join(','))
+    }
+
+    if(shopParams.sort) {
+      params = params.append('sort', shopParams.sort)
+    }
+
+    if (shopParams.search) {
+      params =  params.append('search', shopParams.search)
+    }
+
+    params = params.append('pageSize', shopParams.pageSize)
+    params = params.append('pageIndex', shopParams.pageNumber)
+    // console.log(`params: ${params}`)
+
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {params})
+  }
+
+  //karena langsung subsrcibe, akan dieksekusi ketika pertama kali app hidup
+  //perlu pasang filter biar hanya dieksekusi sekali aja
+  getBrands(){
+    if (this.brands.length > 0) return
+    return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
+      next: response => this.brands = response
+    })
+  }
+
+  getTypes() {
+    if (this.types.length > 0) return
+    return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
+      next: response => this.types = response
+    })
   }
 }
--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -3,25 +3,106 @@ import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/product';
 import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog'
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-shop',
   imports: [
     MatCard,
-    ProductItemComponent
-],
+    ProductItemComponent,
+    MatButton,
+    MatIcon,
+    MatMenu,
+    MatSelectionList,
+    MatListOption,
+    MatMenuTrigger,
+    MatPaginator,
+    FormsModule
+  ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
-export class ShopComponent implements OnInit{
+export class ShopComponent implements OnInit {
   private shopService = inject(ShopService)
-  products: Product[] = [];
+  private dialogService = inject(MatDialog)
+  products?: Pagination<Product>;
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low-High', value: 'priceAsc' },
+    { name: 'price: High-Low', value: 'priceDesc' }
+  ]
+  shopParams = new ShopParams()
+  pageSizeOptions = [5,10,15,20]
 
   ngOnInit(): void {
     //http balikannya observable object. akan jalan klo di subscribe dulu
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
+    this.initializeShop()
+  }
+
+  initializeShop() {
+    this.shopService.getBrands()
+    this.shopService.getTypes()
+    this.getProducts()
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => this.products = response,
       error: error => console.log(error)
+    })
+  }
+
+  onSearchChange () {
+    this.shopParams.pageNumber = 1
+    this.getProducts()
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.shopParams.pageNumber = event.pageIndex + 1
+    this.shopParams.pageSize = event.pageSize
+    this.getProducts()
+  }
+
+  onSortChange(event: MatSelectionListChange) {
+    const selectedOption = event.options[0]
+    if (selectedOption) {
+      this.shopParams.sort = selectedOption.value
+      // console.log(this.shopParams.sort)
+      this.shopParams.pageNumber = 1 //reset
+      this.getProducts()
+    }
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data: {
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types
+      }
+    })
+
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if (result) {
+          console.log(result)
+          this.shopParams.brands = result.selectedBrands
+          this.shopParams.types = result.selectedTypes
+          //apply filters
+          this.shopParams.pageNumber = 1 //reset
+          this.getProducts()
+        }
+      }
     })
   }
 }
